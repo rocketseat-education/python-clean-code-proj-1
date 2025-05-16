a9e536de42ccde93c5cbc3e8f7b967fcb242bd69
offellipe <luis.fellipe.bsilva@gmail.com>
2025-05-15 21:11:27 -0300

    Song register

--- src/main/process_handler.py
+++ src/main/process_handler.py
@@ -1,10 +1,11 @@
 from src.view.first_view import introduction_page
+from .constructor.song_register_contructor import song_register_process
 
 def start() -> None:
     while True:
 
         command = introduction_page()
-        if command == '1': print("Inserindo musica")
+        if command == '1': song_register_process()
         elif command == '2': print("Criando playlist")
         elif command == '5': exit()
         else: print("\n comando não encontrado, tente novamente! \n\n")
