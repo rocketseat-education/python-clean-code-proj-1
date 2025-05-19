688a2e390cb5b328ed98a57f970d8424e9a70c53
offellipe <luis.fellipe.bsilva@gmail.com>
2025-05-18 23:46:20 -0300

    Song Register Validations

--- src/controllers/song_register_controller.py
+++ src/controllers/song_register_controller.py
@@ -1,18 +1,42 @@
 class SongRegisterController:
     def insert(self, new_song_informations: dict) -> dict:
-        self._verify_if_song_already_registered()
-        self._verify_songs_infos()
-        self._insert_song()
-        self._format_response()
+        try:
+            self.__verify_songs_infos(new_song_informations)
+            self.__verify_if_song_already_registered(new_song_informations)
+            self.__insert_song(new_song_informations)
+            self.__format_response(new_song_informations)
 
-    def _verify_if_song_already_registered(new_song_informations: dict) -> None:
+        except Exception as exception:
+            self.__format_error_response(exception)
+
+
+    def __verify_if_song_already_registered(self, new_song_informations: dict) -> None:
+        if len(new_song_informations["title"]) > 100
+            raise Exception("Titulo de musica com mais de 100 caracteres")
+
+        year = int(new_song_informations["year"])
+        if year >= 2026:
+            raise Exception("Ano de musica inválido")
+
+
+    def __verify_songs_infos(self, new_song_informations: dict) -> None:
         pass
 
-    def _verify_songs_infos(new_song_informations: dict) -> None:
+    def __insert_song(self, new_song_information: dict) -> None:
         pass
 
-    def _insert_song() -> None:
-        pass
+    def __format_response(self, new_song_informations: dict) -> dict:
+        return {
+            "success": True,
+            "count": 1,
+            "attributes": {
+                "title": new_song_informations["title"]
+            }
+        }
 
-    def _format_response() -> None:
-        pass
+
+    def __format_error_response(self, err: Exception) -> dict:
+        return {
+            "success": False,
+            "error": str(err)
+        }
